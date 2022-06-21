42d6cede311e478a4f66044b2b755aa412dcfcb4
gilyazov <gilyazov.aydar@gmail.com>
2022-06-21 12:24:40 +0300

    Блок картинка + текст

---
+++ local/blocks/tattelecom/m2m-business-control/.description.php
@@ -0,0 +1,42 @@
+<?php
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+{
+	die();
+}
+
+use \Bitrix\Main\Localization\Loc;
+
+return array(
+	'block' => array(
+        'name' => 'Картинка + текст с кнопкой',
+        'section' => ['Tattelecom', 'banner'],
+        'dynamic' => false,
+	),
+    'cards' => [
+
+    ],
+	'nodes' => array(
+        '.b-page-banner__m2m-title' => array(
+            'name' => 'Заголовок',
+            'type' => 'text',
+        ),
+        '.b-page-banner__m2m-subtitle' => array(
+            'name' => 'Описание',
+            'type' => 'text',
+        ),
+
+        '.b-page-banner__m2m-btn' => array(
+            'name' => 'Кнопка',
+            'type' => 'link',
+        ),
+
+        '.b-page-banner__right-image' => array(
+            'name' => 'Картинка',
+            'type' => 'img',
+            'dimensions' => array('maxWidth' => 500, 'maxHeight' => 500)
+        ),
+	),
+    'groups' => array(
+
+    ),
+);
---
+++ local/blocks/tattelecom/m2m-business-control/block.php
@@ -0,0 +1,17 @@
+<section class="b-page-banner b-m2m-business-control">
+    <div class="gl__page-content">
+        <div class="b-page-banner__content">
+            <div class="b-page-banner__background"></div>
+            <img src="/local/js/template/tattelecom/img/b-m2m-business-control-page/banner.png" alt="" class="b-page-banner__right-image">
+            <div class="b-page-banner__left-content">
+                <h3 class="b-page-banner__m2m-title">Тариф «М2М Бизнес Контроль 2.0»</h3>
+                <div class="b-page-banner__m2m-subtitle">М2М Бизнес Контроль – это тарифный план для передачи данных между устройствами посредством мобильной связи.</div>
+                <div class="b-page-banner__m2m-price-container">
+                    <div class="b-page-banner__m2m-btn-container">
+                        <a href="#" class="b-page-banner__m2m-btn gl__main-btn">Подключить</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
