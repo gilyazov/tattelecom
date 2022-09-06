a4274e4943b806563e62ebf4d0d4dbe89c2c66f7
gilyazov <gilyazov.aydar@gmail.com>
2022-09-06 10:10:30 +0300

    Домофон - первый блок

---
+++ local/blocks/tattelecom/domofon-top-block/.description.php
@@ -0,0 +1,51 @@
+<?php
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+{
+	die();
+}
+
+use \Bitrix\Main\Localization\Loc;
+
+return array(
+	'block' => array(
+        'name' => 'Шапка Домофон',
+        'section' => ['Tattelecom', 'banner'],
+        'dynamic' => false,
+	),
+    'cards' => [
+
+    ],
+	'nodes' => array(
+        '.rates-preview__content-caption' => array(
+            'name' => 'Надзаголовок',
+            'type' => 'text',
+        ),
+        '.gl__title' => array(
+            'name' => 'Заголовок',
+            'type' => 'text',
+        ),
+
+        '.fly-top-block__subtitle-1' => array(
+            'name' => 'Текст 1',
+            'type' => 'text',
+        ),
+        '.fly-top-block__subtitle-2' => array(
+            'name' => 'Текст 2',
+            'type' => 'text',
+        ),
+
+        '.gl__md-hide' => array(
+            'name' => 'Картинка',
+            'type' => 'img',
+            'dimensions' => array('maxWidth' => 1920, 'maxHeight' => 1080)
+        ),
+        '.gl__md-show' => array(
+            'name' => 'Картинка',
+            'type' => 'img',
+            'dimensions' => array('maxWidth' => 1920, 'maxHeight' => 1080)
+        ),
+	),
+    'groups' => array(
+
+    ),
+);
---
+++ local/blocks/tattelecom/domofon-top-block/block.php
@@ -0,0 +1,25 @@
+<section class="domofon-top-block rates-preview" id="fly-top-block-host">
+    <div class="gl__page-content rates-preview__content-wrapper fly-top-block__content-wrapper">
+        <div class="rates-preview__content fly-top-block__content-wrapper__content">
+            <div class="rates-preview__content-caption-wrapper">
+                <div class="rates-preview__content-caption">Умный домофон от Таттелеком</div>
+            </div>
+            <h2 class="gl__title mod-white">Безопасность дома начинается с подъезда</h2>
+
+            <div class="fly-top-block__subtitle">
+                <div class="fly-top-block__subtitle-1">Станьте абонентом Таттелеком,</div>
+                <div class="fly-top-block__subtitle-2">сохранив номер своего мобильного телефона!</div>
+            </div>
+            <?$APPLICATION->IncludeComponent(
+                "tattelecom:form",
+                "domofon-top-block",
+                [],
+                false
+            );?>
+        </div>
+        <div class="rates-preview__img-wrapper">
+            <img src="https://cdn-b2c.dom.ru/b2c-domru/2.246.2/frontend/_next/static/images/header_cover-59cfd164e48197a2d2a74e200a9cdc27.webp" alt="" class="rates-preview__img gl__sm-hide">
+            <img src="/local/js/template/tattelecom-1/img/fly-old-number-page/cards-2.png" alt="" class="rates-preview__img gl__md-show">
+        </div>
+    </div>
+</section>
