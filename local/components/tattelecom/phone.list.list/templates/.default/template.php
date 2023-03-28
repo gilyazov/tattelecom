42ad0e49c55edb9205a2a5c62783e2fa553db60a
gilyazov <gilyazov.aydar@gmail.com>
2023-03-28 15:13:06 +0300

    красивые номера

--- local/components/tattelecom/phone.list.list/templates/.default/template.php
+++ local/components/tattelecom/phone.list.list/templates/.default/template.php
@@ -1,13 +1,15 @@
 <?
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
-if (!$arResult['ITEMS']){
+/** @var array $arParams */
+/** @var array $arResult */
+/*if (!$arResult['ITEMS']){
     return;
-}
+}*/
 ?>
 <?if($_REQUEST['type']) {
     $GLOBALS['APPLICATION']->RestartBuffer();
 } ?>
-
+<?if ($arResult["ITEMS"]):?>
     <ul class="phone-list__list">
         <?foreach ($arResult['ITEMS'] as $arItem):?>
             <li class="phone-list__item">
@@ -25,7 +27,9 @@ if (!$arResult['ITEMS']){
     <div class="phone-list__pagination-wrapper">
         <button type="button" class="phone-list__upload-btn gl__main-btn mod-orange-transparent" data-page="<?=$arParams['PAGE']?>">Загрузить еще</button>
     </div>
-
+<?else:?>
+    <div class="sales-offices-map__not-found mod-show">Пока здесь нет красивых номеров, но скоро они появятся.</div>
+<?endif;?>
 <?if($_REQUEST['type']) {
     die();
 } ?>
