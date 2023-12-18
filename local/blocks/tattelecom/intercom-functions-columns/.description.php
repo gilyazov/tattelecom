a51dfadc689f0937261a9466fd4ae6b540e47179
gilyazov <gilyazov.aydar@gmail.com>
2023-12-18 17:59:08 +0300

    Новый блок 2 колонки преимуществ

---
+++ local/blocks/tattelecom/intercom-functions-columns/.description.php
@@ -0,0 +1,39 @@
+<?php
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+{
+	die();
+}
+
+use \Bitrix\Main\Localization\Loc;
+
+return array(
+	'block' => array(
+        'name' => '2 колонки преимуществ',
+        'section' => ['Tattelecom', 'content'],
+        'dynamic' => false,
+	),
+    'cards' => [
+        '.landing-node-red' => [
+            'name' => 'Первая колонка',
+            'label' => [
+                '.landing-node-red .intercom-functions__item-text'
+            ]
+        ],
+        '.landing-node-black' => [
+            'name' => 'Вторая колонка',
+            'label' => [
+                '.landing-node-black .intercom-functions__item-text'
+            ]
+        ],
+    ],
+	'nodes' => array(
+        '.landing-node-red .intercom-functions__item-text' => array(
+            'name' => 'Преимущество',
+            'type' => 'text',
+        ),
+        '.landing-node-black .intercom-functions__item-text' => array(
+            'name' => 'Преимущество',
+            'type' => 'text',
+        ),
+	)
+);
---
+++ local/blocks/tattelecom/intercom-functions-columns/block.php
@@ -0,0 +1,35 @@
+<section class="intercom-functions" id="intercom-functions-host">
+    <div class="gl__page-content">
+        <div class="intercom-functions__content">
+            <ul class="intercom-functions__list">
+                <li class="intercom-functions__item landing-node-red">
+                    <img src="/local/js/template/tattelecom-1/img/promo-intercom/check.svg" alt="" class="intercom-functions__item-image">
+                    <div class="intercom-functions__item-text">Управление через смартфон, включая открывание двери</div>
+                </li>
+                <li class="intercom-functions__item landing-node-red">
+                    <img src="/local/js/template/tattelecom-1/img/promo-intercom/check.svg" alt="" class="intercom-functions__item-image">
+                    <div class="intercom-functions__item-text">Вы видите все, что происходит за дверью в режиме онлайн</div>
+                </li>
+                <li class="intercom-functions__item landing-node-red">
+                    <img src="/local/js/template/tattelecom-1/img/promo-intercom/check.svg" alt="" class="intercom-functions__item-image">
+                    <div class="intercom-functions__item-text">Электронные ключи, привязанные к конкретной квартире</div>
+                </li>
+            </ul>
+
+            <ul class="intercom-functions__list black-color">
+                <li class="intercom-functions__item landing-node-black">
+                    <img src="/local/js/template/tattelecom-1/img/promo-intercom/check.svg" alt="" class="intercom-functions__item-image">
+                    <div class="intercom-functions__item-text">Управление через смартфон, включая открывание двери</div>
+                </li>
+                <li class="intercom-functions__item landing-node-black">
+                    <img src="/local/js/template/tattelecom-1/img/promo-intercom/check.svg" alt="" class="intercom-functions__item-image">
+                    <div class="intercom-functions__item-text">Вы видите все, что происходит за дверью в режиме онлайн</div>
+                </li>
+                <li class="intercom-functions__item landing-node-black">
+                    <img src="/local/js/template/tattelecom-1/img/promo-intercom/check.svg" alt="" class="intercom-functions__item-image">
+                    <div class="intercom-functions__item-text">Электронные ключи, привязанные к конкретной квартире</div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
