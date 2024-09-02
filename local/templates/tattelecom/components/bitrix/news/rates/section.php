f77a52d397faac1f85edc717bfef41fbfdd4fd12
gilyazov <gilyazov.aydar@gmail.com>
2024-09-02 10:55:41 +0300

    фикс

--- local/templates/tattelecom/components/bitrix/news/rates/section.php
+++ local/templates/tattelecom/components/bitrix/news/rates/section.php
@@ -113,7 +113,7 @@ $this->setFrameMode(true);
                     Array(
                         "IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
                         "IBLOCK_ID" => $arParams["IBLOCK_ID"],
-                        "NEWS_COUNT" => $arParams["NEWS_COUNT"],
+                        "NEWS_COUNT" => 5,
                         "SORT_BY1" => $arParams["SORT_BY1"],
                         "SORT_ORDER1" => $arParams["SORT_ORDER1"],
                         "SORT_BY2" => $arParams["SORT_BY2"],
