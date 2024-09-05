47ad71ef94254e9528f1e37948845eca62832294
gilyazov <gilyazov.aydar@gmail.com>
2024-09-05 14:48:53 +0300

    Слайдер на главной

--- local/templates/tattelecom24/components/bitrix/news.list/rates-preview/template.php
+++ local/templates/tattelecom24/components/bitrix/news.list/rates-preview/template.php
@@ -19,7 +19,7 @@ $this->setFrameMode(true);
             $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
         ?>
         <div class="swiper-slide" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
-            <section class="rates-preview mod-main-page" style="background-image: url('<?=\Tattelecom\Core\Tools::resizeImage($arItem["PREVIEW_PICTURE"]["ID"], 1920, 470)?>')" id="rates-preview-host">
+            <section class="rates-preview mod-main-page" style="background-image: url('<?=\Tattelecom\Core\Tools::resizeImage($arItem["PREVIEW_PICTURE"]["ID"], 2880, 705)?>')" id="rates-preview-host">
                 <div class="gl__page-content rates-preview__content-wrapper">
                     <div class="rates-preview__content <?=$arItem["PROPERTIES"]["COLOR"]["VALUE_XML_ID"]?>">
                         <div class="rates-preview__content-title"><?=$arItem["NAME"]?></div>
@@ -27,7 +27,9 @@ $this->setFrameMode(true);
                         <div class="rates-preview__content-caption-wrapper">
                             <div class="rates-preview__content-caption"><?=$arItem["PROPERTIES"]["DESCRIPTION"]["VALUE"]?></div>
                         </div>
-                        <a href="<?=$arItem["PROPERTIES"]["LINK"]["VALUE"]?>" class="gl__main-btn rates-preview__btn mod-red-btn <?=$arItem["PROPERTIES"]["BTN_COLOR"]["VALUE_XML_ID"]?>">Подробнее</a>
+                        <?if($arItem["PROPERTIES"]["LINK"]["VALUE"]):?>
+                            <a href="<?=$arItem["PROPERTIES"]["LINK"]["VALUE"]?>" class="gl__main-btn rates-preview__btn mod-red-btn <?=$arItem["PROPERTIES"]["BTN_COLOR"]["VALUE_XML_ID"]?>">Подробнее</a>
+                        <?endif;?>
                     </div>
                     <div class="rates-preview__img-wrapper">
                         <img src="<?=\Tattelecom\Core\Tools::resizeImage($arItem["DETAIL_PICTURE"]["ID"], 1920, 470, true)?>" alt="" class="rates-preview__img">
