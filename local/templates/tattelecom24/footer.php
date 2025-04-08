4c5c3b72d2916faef3f237d6b2eaabe09d5f5650
gilyazov <gilyazov.aydar@gmail.com>
2025-04-08 09:41:49 +0300

    актуальная метрика

--- local/templates/tattelecom24/footer.php
+++ local/templates/tattelecom24/footer.php
@@ -958,7 +958,7 @@ if ($page == "/domofon/"):
 <script src="https://api-maps.yandex.ru/2.1/?apikey=978d9dde-738d-4b87-8d8a-1de67b511402&lang=ru_RU"
         type="text/javascript"></script>
 </div>
-
+<?$metrika=(SITE_ID=="s2"?93374862:48331871)?>
 <!-- Yandex.Metrika counter -->
 <script type="text/javascript" >
     (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
@@ -967,14 +967,14 @@ if ($page == "/domofon/"):
         k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
     (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-    ym(48331871, "init", {
+    ym(<?=$metrika?>, "init", {
         clickmap:true,
         trackLinks:true,
         accurateTrackBounce:true,
         webvisor:true
     });
 </script>
-<noscript><div><img src="https://mc.yandex.ru/watch/48331871" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<noscript><div><img src="https://mc.yandex.ru/watch/<?=$metrika?>" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
 
 <?$APPLICATION->ShowProperty('BeforeBodyClose');?>
