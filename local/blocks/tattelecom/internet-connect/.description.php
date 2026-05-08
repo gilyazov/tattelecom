e608f7396b7dfa3336a17fbe00aa1db6c620e97b
gilyazov <gilyazov.aydar@gmail.com>
2026-05-08 14:57:54 +0300

    новый блок проверки возможности подключения

---
+++ local/blocks/tattelecom/internet-connect/.description.php
@@ -0,0 +1,28 @@
+<?php
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+{
+	die();
+}
+
+use \Bitrix\Main\Localization\Loc;
+
+return array(
+	'block' => array(
+        'name' => 'Проверьте возможность подключения по вашему адресу
+',
+        'section' => ['Tattelecom', 'content'],
+        'dynamic' => false,
+	),
+    'cards' => [
+
+    ],
+	'nodes' => array(
+
+	),
+    'style' => array(
+
+    ),
+    'attrs' => array(
+
+    ),
+);
---
+++ local/blocks/tattelecom/internet-connect/block.php
@@ -0,0 +1,10 @@
+<section class="gl__section">
+    <div class="gl__page-content">
+        <?$APPLICATION->IncludeComponent(
+            "tattelecom:internet.connect",
+            "",
+            [],
+            false
+        );?>
+    </div>
+</section>
